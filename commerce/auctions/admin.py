37d2502a6015aa5757f56200bb7e18c57109c9fa
Emile Van Der Heyde <EmileVDH@fmi.co.za>
2020-07-31 14:55:25 +0200

    s1

--- commerce/auctions/admin.py
+++ commerce/auctions/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+
+from .models import User, Listing, Bid, Comment , WatchList, ItemCategory
+
+# Register your models here.
+admin.site.register(User)
+admin.site.register(Listing)
+admin.site.register(Bid)
+admin.site.register(Comment)
+admin.site.register(WatchList)
+admin.site.register(ItemCategory)
+
--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -1,6 +1,61 @@
+
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+
+# store Log in details and passwords for authentication 
+# Also stores the User Information 
+
 
 
 class User(AbstractUser):
     pass
+
+class ItemCategory (models.Model):
+    ItemCategoryName=models.CharField('Cat Name', max_length=20)
+    def __str__(self):
+        return f"{self.ItemCategoryName})"
+
+# store auction listings - create listing 
+class Listing (models.Model):
+    Title=models.CharField(max_length=64)
+    Description = models.TextField(blank=True)
+    StartBidAmount=models.FloatField()
+    Image=models.ImageField(upload_to='listing')
+    ItemCategory=models.ForeignKey(ItemCategory, on_delete=models.CASCADE)
+    ListedOn= models.DateTimeField(auto_now_add=True)
+    Status=models.BooleanField(default=False)
+    ListedBy= models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.Title
+
+    def max_bid(self):
+        data = Listing.objects.filter(id=self.id).annotate(max=models.Max('bid__Amount'))
+        return data[0].max
+
+
+class Bid(models.Model):
+    listing=models.ForeignKey(Listing,on_delete=models.CASCADE)
+    BidBy=models.ForeignKey(User, on_delete=models.CASCADE)
+    Amount=models.FloatField()
+    BidOn= models.DateTimeField(auto_now_add=True)
+
+class Comment(models.Model):
+    listing=models.ForeignKey(Listing,on_delete=models.CASCADE)
+    CommentBy=models.ForeignKey(User, on_delete=models.CASCADE)
+    Comment=models.TextField()
+    CommentOn= models.DateTimeField(auto_now_add=True)
+
+class WatchList(models.Model):
+    listing=models.ForeignKey(Listing,on_delete=models.CASCADE)
+    WatchListOf=models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+
+   
+
+
+
+
+
+
